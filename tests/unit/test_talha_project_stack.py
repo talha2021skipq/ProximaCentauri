fe4e78c1b985cbd6a2db6a693ccacdf13513ebbe
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2021-12-17 05:39:06 +0000

    Sprint1Task3

---
+++ tests/unit/test_talha_project_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from talha_project.talha_project_stack import TalhaProjectStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in talha_project/talha_project_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = TalhaProjectStack(app, "talha-project")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
