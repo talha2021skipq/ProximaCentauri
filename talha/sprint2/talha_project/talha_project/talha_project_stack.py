ad8d8d956d279c0d24d13d7dab5230c7b708aa19
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2021-12-24 05:58:52 +0000

    Stack last cnhddd

--- talha/sprint2/talha_project/talha_project/talha_project_stack.py
+++ talha/sprint2/talha_project/talha_project/talha_project_stack.py
@@ -26,8 +26,9 @@ class TalhaProjectStack(cdk.Stack):
         lambda_role=self.create_lambda_role()
     # The code that defines your stack goes here
         HWlambda=self.create_lambda('FirstHWlambda', './resources','webHealth_talha_lambda.lambda_handler' ,lambda_role)
-
-#        Talha_db_lambda=self.create_lambda('neTwlambda', './resources','talha_dynamoDb_lambda.lambda_handler' ,lambda_role)
+        
+        db_lambda_role = self.create_db_lambda_role()
+        Talha_db_lambda=self.create_lambda('neTwlambda', './resources','talha_dynamoDb_lambda.lambda_handler' ,db_lambda_role)
  
     #Creating an event after every one minute
         lambda_schedule= events_.Schedule.rate(cdk.Duration.minutes(1))
@@ -39,21 +40,21 @@ class TalhaProjectStack(cdk.Stack):
             schedule= lambda_schedule,
             targets=[lambda_target])
         #create table in dynamo db
- #       try:
-  #          dynamo_table= self.create_table()
-   #     except: pass
+        try:
+            dynamo_table= self.create_table()
+        except: pass
         #give read write permissions to our lambda
-#        dynamo_table.grant_read_write_data(Talha_db_lambda)
+        dynamo_table.grant_read_write_data(Talha_db_lambda)
         ###defining SNS service    
         topic = sns.Topic(self, "TalhaSkipQWebHealthTopic")
         #sns subscription with email
- #       topic.add_subscription( subscriptions_.EmailSubscription('talha.naeem.s@skipq.org'))
+        topic.add_subscription( subscriptions_.EmailSubscription('talha.naeem.s@skipq.org'))
         #topic = sns.Topic(self, "TalhaSkipQdynamodbTopic") # NO NEED TO define another TOPIC within one stack
         #topic.add_subscription(subscriptions_.LambdaSubscription(fn=))
 ###Add lambda subscription to db_lambda, whenever an event occurs at the specified topic
-  #      topic.add_subscription(subscriptions_.LambdaSubscription(fn=Talha_db_lambda))
+        topic.add_subscription(subscriptions_.LambdaSubscription(fn=Talha_db_lambda))
         listofurls=s3bucket_url.read_url_list()
-#        self.create_alarm(topic,listofurls)
+        self.create_alarm(topic,listofurls)
         
 #net jump
     #uncomment for creating s3bucket 
@@ -102,6 +103,17 @@ class TalhaProjectStack(cdk.Stack):
                 aws_iam.ManagedPolicy.from_aws_managed_policy_name('CloudWatchFullAccess')
             ]) 
         return lambdaRole##
+    
+    def create_db_lambda_role(self):
+        lambdaRole = aws_iam.Role(self, "lambda-role-db",
+                        assumed_by = aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+                        managed_policies=[
+                            aws_iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSLambdaBasicExecutionRole'),
+                            aws_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonDynamoDBFullAccess'),
+                            aws_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonSNSFullAccess'),
+                        ])
+            
+        return lambdaRole
 
     def create_lambda( self,id,asset,handler, role):#
         return _lambda.Function(self, id,
@@ -119,7 +131,7 @@ class TalhaProjectStack(cdk.Stack):
         return db_.Table(self,id="Table", table_name="TalhaAlarmTable",partition_key=db_.Attribute(name="id", type=db_.AttributeType.STRING), 
             sort_key=db_.Attribute(name="createdDate", type=db_.AttributeType.STRING))
 ####################################################################################################################
-###      Generating Metrics and then raising alarms on them.                                                    ####
+##      Generating Metrics and then raising alarms on them.                                                    ####
 ####################################################################################################################
     def create_alarm(self, topic, URLLS):
         for web in URLLS:
@@ -135,7 +147,6 @@ class TalhaProjectStack(cdk.Stack):
                 evaluation_periods=1, 
                 threshold= constants.THRESHOLD_AVAIL
                 ))
-    '''
     #create a metric class for latency
             latency_metric=(cloudwatch_.Metric(namespace= constants.URL_MONITOR_NAMESPACE, metric_name=web+constants.URL_MONITOR_NAME1L, 
                 dimensions_map=dimension, period=cdk.Duration.minutes(1),label=web+'Latency_metric'))
@@ -151,4 +162,4 @@ class TalhaProjectStack(cdk.Stack):
             availability_alarm.add_alarm_action(actions_.SnsAction(topic))
             latency_alarm.add_alarm_action(actions_.SnsAction(topic))
             
-       ''' 
+        
