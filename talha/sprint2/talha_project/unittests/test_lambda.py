655c5f4ebe15d4a9bc56e7d21d66e5d1320e6ac8
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2021-12-24 15:48:37 +0000

    Update test

--- talha/sprint2/talha_project/unittests/test_lambda.py
+++ talha/sprint2/talha_project/unittests/test_lambda.py
@@ -1,12 +1,12 @@
 import pytest
 from aws_cdk import core
-import aws_cdk.assertions as assertions
+#import aws_cdk.assertions as assertions
 from talha_project.talha_project_stack import TalhaProjectStack
 def test_lambda():
     app=core.App()
     stack=TalhaProjectStack(app, "TestinfraStackTalha")
-    template = assertions.Template.from_stack(stack)
-    #template=app.synth().get_stack_by_name('Test Stack').template
+    #template = assertions.Template.from_stack(stack)
+    template=app.synth().get_stack_by_name('TestSStack').template
     functions= [resource for resource in template['Resources'].values() if resource['Type']=='AWS::Lambda::Function']
     assert len(functions==2)
 
