cf30bee79aa8b04ffaca3a51b962b1694624022e
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2021-12-24 16:23:51 +0000

    Updated again test

--- talha/sprint2/talha_project/unittests/test_lambda.py
+++ talha/sprint2/talha_project/unittests/test_lambda.py
@@ -4,9 +4,9 @@ from aws_cdk import core
 from talha_project.talha_project_stack import TalhaProjectStack
 def test_lambda():
     app=core.App()
-    stack=TalhaProjectStack(app, "infStack")
+    stack=TalhaProjectStack(app, 'infStack')
     #template = assertions.Template.from_stack(stack)
-    template=app.synth().get_stack_by_name('infStack').template
+    template=app.synth().get_stack_by_name('infStack").template
     functions= [resource for resource in template['Resources'].values() if resource['Type']=='AWS::Lambda::Function']
     assert len(functions==2)
 
