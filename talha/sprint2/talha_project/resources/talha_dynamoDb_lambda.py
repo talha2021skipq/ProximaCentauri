c1c465d3b3967b1f03859a98bf6b6837e07c4ea4
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2021-12-25 17:58:25 +0000

    Added Roll Backk

--- talha/sprint2/talha_project/resources/talha_dynamoDb_lambda.py
+++ talha/sprint2/talha_project/resources/talha_dynamoDb_lambda.py
@@ -4,10 +4,10 @@ import constants as constants
 def lambda_handler(events, context):
     print(events)
     db = dynamoTablePutData();#creating an instance of my putdata class
-    message = events['Records'][0]['Sns']['MessageId']
-    message = json.loads(message)
-    parsed_message =  message['AlarmName']
-    createdDate = message['StateChangeTime']
+    message = events['Records'][0]['Sns'] #['Message']
+    msggg = json.loads(message['Message'])
+    parsed_message =  msggg['AlarmName']
+    createdDate = message['Timestamp']#['StateChangeTime']
    # reason=message['ReasonforStateChange']
     if parsed_message[0]=="B":
        table_name="Beta-infraStack-TableCD117FA1-10BTARD7DGVYY"
