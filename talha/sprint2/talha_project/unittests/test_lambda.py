0018a85c9a60b2fed7a455d230ae2fdc001ad641
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2021-12-27 05:16:26 +0000

    added test

--- talha/sprint2/talha_project/unittests/test_lambda.py
+++ talha/sprint2/talha_project/unittests/test_lambda.py
@@ -9,5 +9,13 @@ def test_lambda():
     template=app.synth().get_stack_by_name('infStack').template
     functions= [resource for resource in template['Resources'].values() if resource['Type']=='AWS::Lambda::Function']
     assert len(functions)==2
-
+def test_alarms():
+    app=core.App()
+    stack=TalhaProjectStack(app, 'infStack')
+    #template = assertions.Template.from_stack(stack)
+    template=app.synth().get_stack_by_name('infStack').template
+    functions= [resource for resource in template['Resources'].values() if resource['Type']=='AWS::CloudWatch::Alarm']
+    assert len(functions)==3
+    
+    
     
