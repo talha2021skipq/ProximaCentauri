d3fda06b019558a5ca78c5b8b63704660fcb3a5c
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2021-12-29 07:32:09 +0000

    Up

---
+++ talha/sprint3/talha_project/cdk.out/asset.b6aea89dc95e9265547fbf811f50213252303277f7139393dc5088f78e5ff317/db_putdata.py
@@ -0,0 +1,17 @@
+import boto3
+
+from boto3.dynamodb.conditions import Key
+
+import os
+class dynamoTablePutData:
+    def __init__(self):
+        self.resource = boto3.resource('dynamodb') 
+    def dynamo_data(self, tableName, message, createdDate):
+        #db=boto3.client('dynamodb')
+        table = self.resource.Table(tableName)
+        values = {}
+        values['id'] = message
+        values['createdDate'] = createdDate
+        #values['Reason'] = reason
+        table.put_item(Item = values)
+    #    db.put_item(tableName,Item = values)
