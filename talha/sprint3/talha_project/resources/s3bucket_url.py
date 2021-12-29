7c5feb7e4ef792bcd7de56a8883d86be47c2056a
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2021-12-29 18:03:35 +0000

    added

--- talha/sprint3/talha_project/resources/s3bucket_url.py
+++ talha/sprint3/talha_project/resources/s3bucket_url.py
@@ -3,7 +3,7 @@ import json
 import boto3
 
 
-def read_url_list():
+def write_urls_to_table(tablename):
     s3= boto3.client('s3')
     bucket_talha= "talhabucketnew"
     file_name ="URLS_new.json"
@@ -11,5 +11,23 @@ def read_url_list():
     cont = response['Body']
     json_oject = json.loads(cont.read())
     list_url=[json_oject['w1'],json_oject['w2'],json_oject['w3'],json_oject['w4']]
-    return list_url
-        
+    
+    resource= boto3.resource('dynamodb')
+    table = resource.Table(tablename)
+    for el in list_url:
+        values = {}
+        values['URL'] = el
+        table.put_item(Item = values)
+    
+    return 
+def read_url_list_from_table(tablenamee):
+    resource= boto3.resource('dynamodb')
+    table = resource.Table(tablenamee)
+    table.get_item(Key={"partition_key":"URL"})
+
+    response = table.scan()
+    data = response['Items']
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+    return data
