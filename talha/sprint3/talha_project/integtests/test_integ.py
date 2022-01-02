018b51816b68736a9016787807c3d288702f72c7
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2022-01-02 16:49:30 +0000

    test

--- talha/sprint3/talha_project/integtests/test_integ.py
+++ talha/sprint3/talha_project/integtests/test_integ.py
@@ -1,10 +1,12 @@
+import datetime
 import pytest
 import requests
 import json
 from resources import puturlDB as putdb
 BetaURLtable="Beta-infraStack-URLTable1792207E-1E3WEGLZJ0NFU"
+db=putdb.dynamoTablePutURLData()
 def test_integ():
-    db=putdb.dynamoTablePutURLData()
+    
     urllist=db.rdynamo_data(BetaURLtable)
     ##Getting the URLS in real time using API invoke link
     api_res=requests.get('https://4jd8g9kea3.execute-api.us-east-2.amazonaws.com/prod/')
@@ -13,7 +15,12 @@ def test_integ():
     print("The reply=", reply)
     
     assert len(urllist) == len(reply['body'])
-    
+def realtime_test():
+    start =datetime.datetime.now()
+    api_put_res=requests.put('https://4jd8g9kea3.execute-api.us-east-2.amazonaws.com/prod/', data={"URL","dummy.com"})
+    end=datetime.datetime.now()
+    latency=end-start
+    assert latency==2
      
      
     
