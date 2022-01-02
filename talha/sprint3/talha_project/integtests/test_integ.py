55acbcd2cebfec4d394df5c8448e41fdc066e40d
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2022-01-02 13:14:23 +0000

    Testing intg test

--- talha/sprint3/talha_project/integtests/test_integ.py
+++ talha/sprint3/talha_project/integtests/test_integ.py
@@ -1,3 +1,19 @@
 import pytest
+import requests
+import json
+from resources import puturlDB as putdb
+BetaURLtable="Beta-infraStack-URLTable1792207E-1E3WEGLZJ0NFU"
 def test_integ():
+    db=putdb.dynamoTablePutURLData()
+    urllist=db.rdynamo_data(BetaURLtable)
+    ##Getting the URLS in real time using API invoke link
+    api_res=requests.get('https://4jd8g9kea3.execute-api.us-east-2.amazonaws.com/prod/')
+    reply=json.loads(api_res.text)
+    assert len(urllist) == len(api_res['body'])
+    
+     
+     
+    
+    
     assert 2==2
+# API invoke link: https://4jd8g9kea3.execute-api.us-east-2.amazonaws.com/prod/
