6e9b5990d008f0f7c75bb4f00666d1d19eeac0dc
Ubuntu <ubuntu@ip-172-31-32-14.us-east-2.compute.internal>
2022-01-03 04:56:03 +0000

    testIntgg

--- talha/sprint3/talha_project/integtests/test_integ.py
+++ talha/sprint3/talha_project/integtests/test_integ.py
@@ -5,6 +5,16 @@ import json
 from resources import puturlDB as putdb
 BetaURLtable="Beta-infraStack-URLTable1792207E-1E3WEGLZJ0NFU"
 db=putdb.dynamoTablePutURLData()
+
+def test_realtime():
+    start =datetime.datetime.now()
+    api_put_res=requests.put('https://4jd8g9kea3.execute-api.us-east-2.amazonaws.com/prod/', data={"URL","dummy.com"})
+    end=datetime.datetime.now()
+    dif=end-start
+    latency=round(dif.microseconds * 0.000001,6)
+    assert latency<5
+     
+     
 def test_integ():
     
     urllist=db.rdynamo_data(BetaURLtable)
@@ -15,14 +25,6 @@ def test_integ():
     print("The reply=", reply)
     
     assert len(urllist) == len(reply['body'])
-def realtime_test():
-    start =datetime.datetime.now()
-    api_put_res=requests.put('https://4jd8g9kea3.execute-api.us-east-2.amazonaws.com/prod/', data={"URL","dummy.com"})
-    end=datetime.datetime.now()
-    dif=end-start
-    latency=round(dif.microseconds * 0.000001,6)
-    assert latency<5
-     
-     
+
 #assert 2==2
 # API invoke link: https://4jd8g9kea3.execute-api.us-east-2.amazonaws.com/prod/
